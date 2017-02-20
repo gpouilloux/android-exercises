6225fcfab9165431f50268003510cae881e0379c
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-20 16:15:39 +0100

    [img] display an image using Glide

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -10,8 +13,15 @@ public class LibraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        // TODO Get image view and load image form URL
-        // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        // Get image view and load image form URL
+        final ImageView downloadImageView = (ImageView) findViewById(R.id.downloadImageView);
+        final String imageURL = "http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg";
+        Glide.with(this)
+            .load(imageURL)
+            .centerCrop()
+            .crossFade()
+            .into(downloadImageView);
+        //
     }
 
 }
