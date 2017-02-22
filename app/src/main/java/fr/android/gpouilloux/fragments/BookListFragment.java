2659f924242f24f4da6f4858b5a0f4df2d9f3018
Guillaume Pouilloux <gui.pouilloux@gmail.com>
2017-02-22 12:46:09 +0100

    [book list] add line divider between items

--- app/src/main/java/fr/android/gpouilloux/fragments/BookListFragment.java
+++ app/src/main/java/fr/android/gpouilloux/fragments/BookListFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,6 +15,7 @@ import java.util.List;
 
 import fr.android.gpouilloux.R;
 import fr.android.gpouilloux.adapters.BookRecyclerAdapter;
+import fr.android.gpouilloux.decorations.BasicDividerItemDecoration;
 import fr.android.gpouilloux.models.Book;
 import fr.android.gpouilloux.services.HenriPotierService;
 import retrofit2.Call;
@@ -54,6 +54,7 @@ public class BookListFragment extends Fragment {
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.bookRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.addItemDecoration(new BasicDividerItemDecoration(view.getContext()));
         recyclerView.setAdapter(adapter);
 
         // build Retrofit
